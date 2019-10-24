a520e5eb2c752bb8d2ef58655911290123ba8f96
Leohn <leohnlee@gmail.com>
2019-10-24 18:44:15 +0900

    Auto stash before merge of "flask" and "origin/flask"

--- server/analysis_server_flask/app.py
+++ server/analysis_server_flask/app.py
@@ -1,11 +1,27 @@
 from flask import Flask
 from flask_restful import Api
 from routes.main import *
+from bin.exceptions import * # exception file work like message
+from bin.logger import Logger
+from bin.config import _train_job # model parameters
+from bin.im_models import HierarchicalKMeans, HKMModels # models
 
 app = Flask(__name__)
 api = Api(app)
 
 api.add_resource(UrlAnalyze, '/UrlAnalyze')
+
+# 
+logger = Logger(verbose=True, debug=False)
+
+# params parsing
+params = _train_job["parameters"]
+dir_path = _train_job["paths"]["dir"]["models"]
+file_name = _train_job["paths"]["file"]["model_name"]
+
+# load models
+model = HierarchicalKMeans(parameters = params, logger = logger)
+model.load(dir_path = dir_path, file_name = file_name)
 
 if __name__ == '__main__':
     app.run(debug=True, port = 5000)
